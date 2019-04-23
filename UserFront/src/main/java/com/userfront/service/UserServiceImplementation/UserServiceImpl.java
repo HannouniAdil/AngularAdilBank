061ea379dac61e45ee80bb030acc7c251d6a0471
adilhannouni <adilhannouni@gmail.com>
2019-04-22 17:03:37 -0700

    Add first project load 30% done

---
+++ UserFront/src/main/java/com/userfront/service/UserServiceImplementation/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.userfront.service.UserServiceImplementation;
+
+import org.springframework.stereotype.Service;
+
+import com.userfront.service.UserService;
+
+//register as a service bean so springboot register the bean in the DB 
+@Service
+public class UserServiceImpl implements UserService {
+
+}
