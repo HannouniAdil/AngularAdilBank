3bcdfaadf2d010b1c05b07eadde7204400d29e2b
adilhannouni <adilhannouni@gmail.com>
2019-04-26 01:16:18 -0700

    Add User Service and User DAO

--- UserFront/src/main/java/com/userfront/controller/HomeController.java
+++ UserFront/src/main/java/com/userfront/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.userfront.controller;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,10 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.userfront.domain.User;
+import com.userfront.service.UserService;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping("/")
 	public String home() {
@@ -32,27 +38,31 @@ public class HomeController {
     }
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public void signupPost(@ModelAttribute("user") User user,  Model model) {
+    public String signupPost(@ModelAttribute("user") User user,  Model model) {
 
-//        if(userService.checkUserExists(user.getUsername(), user.getEmail()))  {
-//
-//            if (userService.checkEmailExists(user.getEmail())) {
-//                model.addAttribute("emailExists", true);
-//            }
-//
-//            if (userService.checkUsernameExists(user.getUsername())) {
-//                model.addAttribute("usernameExists", true);
-//            }
-//
-//            return "signup";
-//        } else {
-//        	 Set<UserRole> userRoles = new HashSet<>();
-//             userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
-//
-//            userService.createUser(user, userRoles);
-//
-//            return "redirect:/";
-//        }
+		String response = null;
+		
+        if(userService.checkUserExists(user.getUsername(), user.getEmail()))  {
+
+            if (userService.checkEmailExists(user.getEmail())) {
+                model.addAttribute("emailExists", true);
+            }
+
+            if (userService.checkUsernameExists(user.getUsername())) {
+                model.addAttribute("usernameExists", true);
+            }
+
+            response =  "signup";
+        } else {
+        	// Set<UserRole> userRoles = new HashSet<>();
+            // userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
+
+           // userService.createUser(user, userRoles);
+        	userService.save(user);
+
+            response =  "redirect:/";
+        }
+        return response;
     }
 //	
 //	@RequestMapping("/userFront")
--- UserFront/src/main/java/com/userfront/dao/UserDao.java
+++ UserFront/src/main/java/com/userfront/dao/UserDao.java
@@ -1,5 +1,12 @@
 package com.userfront.dao;
 
-public interface UserDao {
+import org.springframework.data.repository.CrudRepository;
 
+import com.userfront.domain.User;
+
+public interface UserDao extends CrudRepository<User, Long>{
+	
+	User findByUsername(String username);
+	User findByEmail(String email);
+	
 }
